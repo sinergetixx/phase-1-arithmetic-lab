43258546d19f2e758f27f0ee4f2f2483626b12ad
rob <robertmaticke12@gmail.com>
2022-08-11 10:10:00 -0400

    commit tree 9c31b930e4273dc8a918d0a72ce012dc1e63c33a

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
 
 // Write your code here
+let num1 = 2
+let num2 = 31
+let multiply = num1*num2
+
+let random = Math.ceil(Math.random())
+
+let num3 = 16
+let num4 = 6
+let mod = num3%num4
+
+let max = Math.max(2, 6, 12, 20)
